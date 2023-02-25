645e57bdff1088430b68ff00b2745a8e3f223f47
ASilyutina <114189382+ASilyutina@users.noreply.github.com>
2023-02-25 13:28:22 +0300

    5 commit

---
+++ 4.1_task1.php
@@ -0,0 +1,36 @@
+<?php
+$name = 'Иннокентий';
+$prof = 'Дизайнер';
+$country = 'Российская Федерация';
+$city = 'Москва';
+$email = 'email@email.com';
+$tel = '89037778866';
+
+$skills = [
+  ['name' => 'Adobe Photoshop', 'percent' => 92],
+  ['name' => 'Photo', 'percent' => 80],
+  ['name' => 'Illustrator', 'percent' => 75],
+  ['name' => 'Media', 'percent' => 50],
+];
+
+$workExperience = [
+  [
+    'jobTitle' => 'Front End Developer',
+    'periodStart' => 'Jan 2015',
+    'periodFinish' => 'Current',
+    'description' => 'Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste'
+  ],
+  [
+    'jobTitle' => 'Front End Developer',
+    'periodStart' => 'Mar 2012',
+    'periodFinish' => 'Dec 2014',
+    'description' => 'Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.'
+  ],
+  [
+    'jobTitle' => 'Graphic Designer / designsomething.com',
+    'periodStart' => 'Jun 2010',
+    'periodFinish' => 'Mar 2012',
+    'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+  ],
+];
+?>
